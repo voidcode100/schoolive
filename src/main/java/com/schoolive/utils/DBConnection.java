244e8a51c2cba895fa9712a7e32fe2ede25a4ae3
voidcode100 <spunckcore@gamil.com>
2025-05-02 15:39:35 +0800

    fix DB-driver

--- src/main/java/com/schoolive/utils/DBConnection.java
+++ src/main/java/com/schoolive/utils/DBConnection.java
@@ -1,15 +1,32 @@
 package com.schoolive.utils;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/schoolive";
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/schoolive?useSSL=false&serverTimezone=UTC&useUnicode=true&characterEncoding=UTF-8";
     private static final String USER = "root";
     private static final String PASSWORD = "123456";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    static {
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static void close(Connection conn, Statement stmt, ResultSet rs) {
+        try {
+            if (rs != null) rs.close();
+            if (stmt != null) stmt.close();
+            if (conn != null) conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
